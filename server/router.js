35c32a576f2c159e0b53571e4136da15dc568417
Gilbert Macabuag <gmacabuag@mbsbooks.com>
2019-02-28 12:01:03 -0500

    Finish coding for server app

--- server/router.js
+++ server/router.js
@@ -1,5 +1,15 @@
 const AuthenticationController = require("./controllers/authentication");
+const passportService = require("./services/passport");
+const passport = require("passport");
+
+const requireAuth = passport.authenticate("jwt", { session: false });
+const requireSignin = passport.authenticate("local", { session: false });
 
 module.exports = function(app) {
+  app.get("/", requireAuth, function(req, res) {
+    res.send({ hi: "there" });
+  });
+
+  app.post("/signin", requireSignin, AuthenticationController.signin);
   app.post("/signup", AuthenticationController.signup);
 };
--- server/services/passport.js
+++ server/services/passport.js
@@ -3,6 +3,36 @@ const User = require("../models/user");
 const config = require("../config/secret");
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
+const LocalStrategy = require("passport-local");
+
+// create local strategy
+const localOptions = { usernameField: "email" };
+const localLogin = new LocalStrategy(localOptions, function(
+  email,
+  password,
+  done
+) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) {
+      return done(err);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+
+    // compare passwords
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) {
+        return done(err);
+      }
+      if (!isMatch) {
+        return done(null, false);
+      }
+
+      return done(null, user);
+    });
+  });
+});
 
 // setup options for JWT strategy
 const jwtOptions = {
@@ -27,3 +57,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // tell passport to use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
